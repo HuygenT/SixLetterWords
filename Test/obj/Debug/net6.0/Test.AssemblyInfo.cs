a6e1e24ff460c39b072d2e4a2d4a1e0993122960
Tristan Huygen <tristan.huygen@student.kdg.be>
2024-04-10 11:21:53 +0200

    removed packages

--- Test/obj/Debug/net6.0/Test.AssemblyInfo.cs
+++ Test/obj/Debug/net6.0/Test.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aee5f2e5b94661c2a5c338f246b65a5c180b251d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
